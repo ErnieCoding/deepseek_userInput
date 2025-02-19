5a57df861e0a277f7579bf7464364f5b63e42883
Ernest Saakian <saakianernie@gmail.com>
2025-02-19 23:40:14 +0000

    removed client logic, added requirements.txt

--- deepseek.py
+++ deepseek.py
@@ -1,8 +1,11 @@
 import argparse
 import re
 
-from ollama import chat
-from ollama import ChatResponse
+#from ollama import Client
+
+from ollama import chat, ChatResponse
+
+#client = Client(host="http://127.0.0.1:11411")
 
 def ask(system_prompt, ds_model, deepthink=True, print_log=True):
     response: ChatResponse = chat(
@@ -14,13 +17,13 @@ def ask(system_prompt, ds_model, deepthink=True, print_log=True):
     response_text = response['message']['content']
     if print_log: print(response_text)
 
-    thinkProcess = re.findall(r'<think>(.*?)</think>', response_text, flags=re.DOTALL)
+    #thinkProcess = re.findall(r'<think>(.*?)</think>', response_text, flags=re.DOTALL)
 
-    thinkProcess = "\n\n".join(thinkProcess).strip()
+    #thinkProcess = "\n\n".join(thinkProcess).strip()
 
-    clean = re.sub(r'<think>.*?</think>', '', response_text, flags=re.DOTALL).strip()
+    #clean = re.sub(r'<think>.*?</think>', '', response_text, flags=re.DOTALL).strip()
 
-    return clean
+    return response_text
 
 parser = argparse.ArgumentParser(description="User prompt")
 parser.add_argument("input", type=str, help="The string to be used as prompt")
