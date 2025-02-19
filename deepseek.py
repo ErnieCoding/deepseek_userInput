79160669c7762110bb298a97062eece7329f4550
Ernest <saakianernie@gmail.com>
2025-02-19 12:21:23 -0500

    Script for input processing

---
+++ deepseek.py
@@ -0,0 +1,38 @@
+import argparse
+import re
+
+from ollama import chat
+from ollama import ChatResponse
+
+parser = argparse.ArgumentParser(description="User prompt")
+parser.add_argument("input", type=str, help="The string to be used as prompt")
+
+args = parser.parse_args()
+model = "deepseek-r1:14b"
+
+
+
+response = ask(args.input, model)
+
+print(response)
+
+print('\n')
+
+
+def ask(system_prompt, ds_model, deepthink=True, print_log=True):
+    response: ChatResponse = chat(
+        model = ds_model, messages=[
+            {'role':'user', 'content':system_prompt},
+        ]
+    )
+
+    response_text = response['message']['content']
+    if print_log: print(response_text)
+
+    thinkProcess = re.findall(r'<think>(.*?)</think>', response_text, flags=re.DOTALL)
+
+    thinkProcess = "\n\n".join(thinkProcess).strip()
+
+    clean = re.sub(r'<think>.*?</think>', '', response_text, flags=re.DOTALL).strip()
+
+    return clean
